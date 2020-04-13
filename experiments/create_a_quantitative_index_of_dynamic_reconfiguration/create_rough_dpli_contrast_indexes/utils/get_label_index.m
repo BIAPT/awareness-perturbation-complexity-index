a882816ba7b8cf859fda42e66a449e05090b7d78
Yacine Mahdid <yacine.mahdid@mail.mcgill.ca>
2020-04-13 10:09:07 -0400

    document get_label_index helper function

--- experiments/create_a_quantitative_index_of_dynamic_reconfiguration/create_rough_dpli_contrast_indexes/utils/get_label_index.m
+++ experiments/create_a_quantitative_index_of_dynamic_reconfiguration/create_rough_dpli_contrast_indexes/utils/get_label_index.m
@@ -1,4 +1,12 @@
 function [label_index] = get_label_index(label, location)
+% GET LABEL INDEX is a helper function to find a given label inside a
+% location structure (brute force)
+% 
+% label: a string label to find
+% location: a 1*N channel location structure array from eeglab
+
+    % Iterate over each location and double check if they are the one we
+    % are looking for
     label_index = 0;
     for i = 1:length(location)
        if(strcmp(label,location{i}))
@@ -6,4 +14,5 @@ function [label_index] = get_label_index(label, location)
           return
        end
     end
+    
 end
