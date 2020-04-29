d9d762455f980ff27e11278bab0234bb1cc2f5df
Yacine Mahdid <yacine.mahdid@mail.mcgill.ca>
2020-04-29 10:08:14 -0400

    add weighted contrast matrix

--- projects/create_a_quantitative_index_of_dynamic_reconfiguration/augment_dri_power_by_including_hub_and_better_dpli/README.md
+++ projects/create_a_quantitative_index_of_dynamic_reconfiguration/augment_dri_power_by_including_hub_and_better_dpli/README.md
@@ -5,6 +5,7 @@ This is the report for the milestone `Augment DRI Power by Including Hub and Bet
 
 ## Table of Content
 - [dPLI Dynamic Reconfiguration Index](#dpli-dynamic-reconfiguration-index)
+  - [Weighted FP Contrast Matrix](#weighted-fp-contrast-matrix)
   - [Attempt #6](#attempt-6)
 
 ## dPLI Dynamic Reconfiguration Index
@@ -12,6 +13,74 @@ The current version of the dpli-dri make use of contrast matrices and filters ou
 
 ![dPLI DRI Attempt #5](../.figure/dpli_dri_5.png)
 
+## Weighted FP Contrast Matrix
+Here for each participant we first filtered out all regions except the fronto-parietal ones. Then we applied the same algorithm to weight the resulting matrix than before with a `shift_weight` parameter which takes on the following values (1, 2, 10 or 100).
+
+The algorithm is as follows:
+```matlab
+    % Here we shift the matrix1 matrix2 to check for crossing of the 0.5
+    % mark
+    shift_matrix1 = matrix1 - 0.5;
+    shift_matrix2 = matrix2 - 0.5;
+    
+    % Here we want to have make a matrix that will give us a 1 for crossing
+    % over and a 0 for not crossing over
+    % We check which index in both shifted matrix are positive
+    pos_matrix1 = shift_matrix1 > 0;
+    pos_matrix2 = shift_matrix2 > 0;
+    
+    % We then add these two, we will get a value of 1 (one positive one
+    % negative), 2 (both positive) or 0 (both negative)
+    sign_matrix = pos_matrix1 + pos_matrix2;
+    
+    % To get the amount of crossing we put zeros everywhere and then only
+    % modify the cross matrix for the index that are actually crossing.
+    amount_crossing_matrix = abs(shift_matrix1 - shift_matrix2);
+    cross_matrix = amount_crossing_matrix.*(sign_matrix == 1);
+    
+    % Finally to calculate the weight matrix we put the cross matrix
+    % through the tanh function. Should give 0 for 0 values and a positive
+    % value for positive input saturating at 1. We then shift that matrix
+    % by 1 and weight it by the shift_weight. This will give us a 1 for the
+    % region which don't cross and a scaling proportional to the amount of
+    % crossing for actual cross.
+    weight_matrix = shift_weight*tanh(cross_matrix) + 1;
+    
+    % We finally multiply the naive version of the contrast matrix with
+    % the weight matrix.
+    contrast_matrix = abs(matrix1 - matrix2).*weight_matrix;
+```
+
 We will mix the attempt 3 (which was weighting the contrast matrix more heavily if they were doing a switch lead/lag) with attempt 5 which was only taking the fronto-parietal regions into consideration.
+
+We get the resulting matrix for a sample of two participant at each of the 4 `shift_weight` values (the rest of the participant have a similar dynamic). These were generated with `ex_21a_generate_dpli_contrast_matrix_only_fp_weighted.m`
+
+### WSAS09
+shift_weight = 1:
+![WSAS09 weighted at 1 fp contrast matrix](./.figure/fp_weighted/WSAS09_alpha_weighted_1_contrast_fp_dpli.png)
+
+shift_weight = 2:
+![WSAS09 weighted at 2 fp contrast matrix](./.figure/fp_weighted/WSAS09_alpha_weighted_2_contrast_fp_dpli.png)
+
+shift_weight = 10:
+![WSAS09 weighted at 10 fp contrast matrix](./.figure/fp_weighted/WSAS09_alpha_weighted_10_contrast_fp_dpli.png)
+
+shift_weight = 100:
+![WSAS09 weighted at 100 fp contrast matrix](./.figure/fp_weighted/WSAS09_alpha_weighted_100_contrast_fp_dpli.png)
+
+### WSAS20
+shift_weight = 1:
+![WSAS20 weighted at 1 fp contrast matrix](./.figure/fp_weighted/WSAS20_alpha_weighted_1_contrast_fp_dpli.png)
+
+shift_weight = 2:
+![WSAS20 weighted at 2 fp contrast matrix](./.figure/fp_weighted/WSAS20_alpha_weighted_2_contrast_fp_dpli.png)
+
+shift_weight = 10:
+![WSAS20 weighted at 10 fp contrast matrix](./.figure/fp_weighted/WSAS20_alpha_weighted_10_contrast_fp_dpli.png)
+
+shift_weight = 100:
+![WSAS20 weighted at 100 fp contrast matrix](./.figure/fp_weighted/WSAS20_alpha_weighted_100_contrast_fp_dpli.png)
+
+As expected we get plot with higher contrast as we move up weighting. We should try all the weighting when creating the dpli-dri, however the choice of weight here is still a bit arbitrary.
 
 ### Attempt 6
--- projects/create_a_quantitative_index_of_dynamic_reconfiguration/augment_dri_power_by_including_hub_and_better_dpli/ex_21_improve_fp_with_directionality_weighting/ex_21a_generate_dpli_contrast_matrix_only_fp_weighted.m
+++ projects/create_a_quantitative_index_of_dynamic_reconfiguration/augment_dri_power_by_including_hub_and_better_dpli/ex_21_improve_fp_with_directionality_weighting/ex_21a_generate_dpli_contrast_matrix_only_fp_weighted.m
@@ -4,8 +4,8 @@
 
 %% Experiment Variables
 IN_DIR = "/media/yacine/My Book/datasets/consciousness/Dynamic Reconfiguration Index/";
-OUT_DIR = "/media/yacine/My Book/result_dri/fp_only/";
-MAP_FILE = "/home/yacine/Documents/BIAPT/awareness-perturbation-complexity-index/experiments/create_a_quantitative_index_of_dynamic_reconfiguration/data/bp_to_egi_mapping_yacine.csv";
+OUT_DIR = "/media/yacine/My Book/result_dri/fp_w/";
+MAP_FILE = "/home/yacine/Documents/BIAPT/awareness-perturbation-complexity-index/projects/create_a_quantitative_index_of_dynamic_reconfiguration/data/bp_to_egi_mapping_yacine.csv";
 
 % Esthetic Variables
 COLOR = "hot";
@@ -13,64 +13,72 @@ COLOR = "hot";
 % Here we will skip participant 17 since we do not have recovery
 P_ID = {'WSAS02','WSAS05', 'WSAS09', 'WSAS10', 'WSAS11', 'WSAS12', 'WSAS13', 'WSAS18', 'WSAS19', 'WSAS20', 'WSAS22'};
 
+% this is used in the definition of the contrast matrix to scale the tanh function
+% We are using 1 2 10 100 weights to be able to see if there is a big
+% difference in the matrices generated
+shift_weights = [1 2 10 100];
+
 %% Creating the figures
 % Here we iterate over each participant and each epochs to create the 3
 % subplots per figure
-for p = 1:length(P_ID)
-    participant = P_ID{p};
-    disp(participant);
+for w = 1:length(shift_weights)
+    SHIFT_WEIGHT = shift_weights(w);
+    for p = 1:length(P_ID)
+        participant = P_ID{p};
+        disp(participant);
 
-    % Process each of the three states, since participant WSAS02 is special
-    % in the sense that is has the Brain Product headset we check for it
-    % to processing it correctly
-    if strcmp(participant, "WSAS02")
-        [baseline_r_dpli, baseline_r_location, baseline_r_regions] = process_bp_dpli(strcat(IN_DIR,participant,filesep,'baseline_alpha_dpli.mat'), MAP_FILE);
-        [anesthesia_r_dpli, anesthesia_r_location, anesthesia_r_regions] = process_bp_dpli(strcat(IN_DIR,participant,filesep,'anesthesia_alpha_dpli.mat'), MAP_FILE);
-        [recovery_r_dpli, recovery_r_location, recovery_r_regions] = process_bp_dpli(strcat(IN_DIR,participant,filesep,'recovery_alpha_dpli.mat'), MAP_FILE);
-    else
-        [baseline_r_dpli, baseline_r_location, baseline_r_regions] = process_dpli(strcat(IN_DIR,participant,filesep,'baseline_alpha_dpli.mat'));
-        [anesthesia_r_dpli, anesthesia_r_location, anesthesia_r_regions] = process_dpli(strcat(IN_DIR,participant,filesep,'anesthesia_alpha_dpli.mat'));
-        [recovery_r_dpli, recovery_r_location, recovery_r_regions] = process_dpli(strcat(IN_DIR,participant,filesep,'recovery_alpha_dpli.mat'));        
+        % Process each of the three states, since participant WSAS02 is special
+        % in the sense that is has the Brain Product headset we check for it
+        % to processing it correctly
+        if strcmp(participant, "WSAS02")
+            [baseline_r_dpli, baseline_r_location, baseline_r_regions] = process_bp_dpli(strcat(IN_DIR,participant,filesep,'baseline_alpha_dpli.mat'), MAP_FILE);
+            [anesthesia_r_dpli, anesthesia_r_location, anesthesia_r_regions] = process_bp_dpli(strcat(IN_DIR,participant,filesep,'anesthesia_alpha_dpli.mat'), MAP_FILE);
+            [recovery_r_dpli, recovery_r_location, recovery_r_regions] = process_bp_dpli(strcat(IN_DIR,participant,filesep,'recovery_alpha_dpli.mat'), MAP_FILE);
+        else
+            [baseline_r_dpli, baseline_r_location, baseline_r_regions] = process_dpli(strcat(IN_DIR,participant,filesep,'baseline_alpha_dpli.mat'));
+            [anesthesia_r_dpli, anesthesia_r_location, anesthesia_r_regions] = process_dpli(strcat(IN_DIR,participant,filesep,'anesthesia_alpha_dpli.mat'));
+            [recovery_r_dpli, recovery_r_location, recovery_r_regions] = process_dpli(strcat(IN_DIR,participant,filesep,'recovery_alpha_dpli.mat'));        
+        end
+
+        % Get the common location
+        [common_location, common_region] = get_subset(baseline_r_location, anesthesia_r_location, recovery_r_location, baseline_r_regions, anesthesia_r_regions, recovery_r_regions);
+
+        % Filter the matrices to have the same size
+        baseline_f_dpli = filter_matrix(baseline_r_dpli, baseline_r_location, common_location);
+        anesthesia_f_dpli = filter_matrix(anesthesia_r_dpli, anesthesia_r_location, common_location);
+        recovery_f_dpli = filter_matrix(recovery_r_dpli, recovery_r_location, common_location);
+
+        % Filter the matrices to keep only the frontal and parietal regions
+        [baseline_f_dpli, x_region, y_region] = filter_fp_regions(baseline_f_dpli, common_region);
+        [anesthesia_f_dpli, ~, ~] = filter_fp_regions(anesthesia_f_dpli, common_region);
+        [recovery_f_dpli, ~, ~] = filter_fp_regions(recovery_f_dpli, common_region);
+
+
+        % Calculate the contrast matrix
+        % it should be weighting more the shift from front/back of the brain
+        baseline_vs_recovery = calculate_contrast_matrix(baseline_f_dpli, recovery_f_dpli, SHIFT_WEIGHT);
+        baseline_vs_anesthesia = calculate_contrast_matrix(baseline_f_dpli, anesthesia_f_dpli, SHIFT_WEIGHT);
+        recovery_vs_anesthesia = calculate_contrast_matrix(recovery_f_dpli, anesthesia_f_dpli, SHIFT_WEIGHT);
+
+        % This vector is used for nomarlization
+        sim_all = [baseline_vs_recovery(:); baseline_vs_anesthesia(:); recovery_vs_anesthesia(:)];
+
+        % Here we create the figure that will be saved
+        handle = figure;
+        subplot(1,3,1)
+        plot_pli_diff_axis(baseline_vs_recovery, x_region, y_region, sim_all, COLOR)
+        title(sprintf("%s alpha FP-W(%d) B vs R", participant, SHIFT_WEIGHT))
+        subplot(1,3,2)
+        plot_pli_diff_axis(baseline_vs_anesthesia, x_region, y_region, sim_all, COLOR)
+        title(sprintf("%s alpha FP-W(%d) B vs A", participant, SHIFT_WEIGHT))
+        subplot(1,3,3)
+        plot_pli_diff_axis(recovery_vs_anesthesia, x_region, y_region, sim_all, COLOR)
+        title(sprintf("%s alpha FP-W(%d) R vs A", participant, SHIFT_WEIGHT))
+        colorbar;
+        set(handle, 'Position', [70,152,1527,589]);
+
+        filename = sprintf("%s%s_alpha_weighted_%d_contrast_fp_dpli.png", OUT_DIR, participant, SHIFT_WEIGHT);
+        saveas(handle,filename);
+        close all;    
     end
-    
-    % Get the common location
-    [common_location, common_region] = get_subset(baseline_r_location, anesthesia_r_location, recovery_r_location, baseline_r_regions, anesthesia_r_regions, recovery_r_regions);
-    
-    % Filter the matrices to have the same size
-    baseline_f_dpli = filter_matrix(baseline_r_dpli, baseline_r_location, common_location);
-    anesthesia_f_dpli = filter_matrix(anesthesia_r_dpli, anesthesia_r_location, common_location);
-    recovery_f_dpli = filter_matrix(recovery_r_dpli, recovery_r_location, common_location);
-    
-    % Filter the matrices to keep only the frontal and parietal regions
-    [baseline_f_dpli, x_region, y_region] = filter_fp_regions(baseline_f_dpli, common_region);
-    [anesthesia_f_dpli, ~, ~] = filter_fp_regions(anesthesia_f_dpli, common_region);
-    [recovery_f_dpli, ~, ~] = filter_fp_regions(recovery_f_dpli, common_region);
-    
-    % Calculate a contrast matrix, this is different than before.
-    % now we want to have high score for large differences and low
-    % score for similarities
-    baseline_vs_recovery = abs(baseline_f_dpli - recovery_f_dpli);
-    baseline_vs_anesthesia = abs(baseline_f_dpli - anesthesia_f_dpli);
-    recovery_vs_anesthesia = abs(recovery_f_dpli - anesthesia_f_dpli);
-    
-    % This vector is used for nomarlization
-    sim_all = [baseline_vs_recovery(:); baseline_vs_anesthesia(:); recovery_vs_anesthesia(:)];
-
-    % Here we create the figure that will be saved
-    handle = figure;
-    subplot(1,3,1)
-    plot_pli_diff_axis(baseline_vs_recovery, x_region, y_region, sim_all, COLOR)
-    title(strcat(participant, " alpha (only FP) baseline vs recovery"));
-    subplot(1,3,2)
-    plot_pli_diff_axis(baseline_vs_anesthesia, x_region, y_region, sim_all, COLOR)
-    title(strcat(participant, " alpha (only FP) baseline vs anesthesia"));
-    subplot(1,3,3)
-    plot_pli_diff_axis(recovery_vs_anesthesia, x_region, y_region, sim_all, COLOR)
-    title(strcat(participant, " alpha (only FP) recovery vs anesthesia"));
-    colorbar;
-    set(handle, 'Position', [70,152,1527,589]);
-    
-    filename = strcat(OUT_DIR, participant, "_alpha_contrast_fp_dpli.png");
-    saveas(handle,filename);
-    close all;    
 end
